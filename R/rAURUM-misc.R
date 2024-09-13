2e8db12ff0bdbf626b975bfb2620751b6525bacb
alexpate30 <alexander.pate@manchester.ac.uk>
2024-09-13 11:25:04 +0100

    add gold queries

--- R/rAURUM-misc.R
+++ R/rAURUM-misc.R
@@ -1,3 +1,3 @@
 ### Deal with global variables note
-utils::globalVariables(c("set", "patid", "obsdate", "indexdt", "value", "total", "count", "med", "drug",
+utils::globalVariables(c("set", "patid", "obsdate", "eventdate", "indexdt", "value", "total", "count", "med", "drug", "data2", "data3", "data5", "data6",
                          "dob", "smoking", "var_time"))
